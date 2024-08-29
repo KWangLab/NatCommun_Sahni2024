ff9897fffcebdae3141abb9ce1068c6491554a7d
Sahil Sahni <86493572+sahil-sahni@users.noreply.github.com>
2024-08-28 20:29:55 -0400

    Update calculate_score.R

--- 4-Figure/code/calculate_score.R
+++ 4-Figure/code/calculate_score.R
@@ -1,20 +1,3 @@
-calculate_score <- function(test, name, features){ #
-  input_name = strsplit(name, '_') %>% unlist(.) %>% .[[1]] 
-  features_name = strsplit(names(features),'_') %>% sapply(.,'[[',1) %>% unlist(.)
-  
-  train = features[which(input_name != features_name)] #ensure ensembl model is not training and testing with the same cohort
-  
-  ensembl_df=lapply(1:length(train), function(r){ #calculate score for each ensembl model
-    model_score = test[,-1:-2] %>% .[colnames(.) %in% train[[r]]] %>% apply(1, sum) 
-    model_score = model_score/length(train[[r]])
-    return(model_score)
-  })
-  ensembl_df=do.call(cbind, ensembl_df)
-  
-  score_df = ensembl_df %>% apply(1,function(x){sum(x,na.rm=T)}) %>% data.frame(sample = test[,1], cohort=name, response=test[,2], score=./length(train)) ###NEW /length(train)
-  
-  return(score_df)
-}
 calculate_score_surv <- function(test, name, features){ #
   input_name = strsplit(name, '_') %>% unlist(.) %>% .[[1]] 
   features_name = strsplit(names(features),'_') %>% sapply(.,'[[',1) %>% unlist(.)
@@ -32,7 +15,7 @@ calculate_score_surv <- function(test, name, features){ #
   
   return(score_df)
 }
-calculate_score_scaled <- function(test, name, features){
+calculate_score <- function(test, name, features){
   input_name = strsplit(name, '_') %>% unlist(.) %>% .[[1]] 
   features_name = strsplit(names(features),'_') %>% sapply(.,'[[',1) %>% unlist(.)
   
